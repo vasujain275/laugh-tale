5debf2fa3c506135db7e500fb2e4f558deff7a29
vasujain275 <vasujain275@gmail.com>
2025-03-23 15:36:52 +0530

    Added new py lsp

--- .config/nvim/lua/vj/plugins/lsp.lua
+++ .config/nvim/lua/vj/plugins/lsp.lua
@@ -226,7 +226,22 @@ return {
       tailwindcss = {
         filetypes = { 'html', 'javascriptreact', 'typescriptreact', 'vue', 'svelte' },
       },
-      pyright = {},
+      pylsp = {
+        settings = {
+          pylsp = {
+            plugins = {
+              pyflakes = { enabled = false },
+              pycodestyle = { enabled = false },
+              autopep8 = { enabled = false },
+              yapf = { enabled = false },
+              mccabe = { enabled = false },
+              pylsp_mypy = { enabled = false },
+              pylsp_black = { enabled = false },
+              pylsp_isort = { enabled = false },
+            },
+          },
+        },
+      },
       emmet_ls = {
         filetypes = {
           'html',
